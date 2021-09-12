5cedbb8081db240674218a4f4d90f1d3df48ad04
Kevin Oliveira <kevin.zero@hotmail.com>
2021-09-12 19:26:06 -0300

    Updates

---
+++ precache-manifest.e789c3c450fe8b0d6f34d8a457c947a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1a1849563008bdf286ebafcec730b3",
+    "url": "/project-recipes-app/index.html"
+  },
+  {
+    "revision": "6f138f613fbf1e8ae8ee",
+    "url": "/project-recipes-app/static/css/main.6cfa15ad.chunk.css"
+  },
+  {
+    "revision": "078f88182ad89e3f30f4",
+    "url": "/project-recipes-app/static/js/2.acc31ab1.chunk.js"
+  },
+  {
+    "revision": "a4061f4e46b505543d7a51823b95f6d9",
+    "url": "/project-recipes-app/static/js/2.acc31ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f138f613fbf1e8ae8ee",
+    "url": "/project-recipes-app/static/js/main.b29ff519.chunk.js"
+  },
+  {
+    "revision": "ae08d26951d31acaaa73",
+    "url": "/project-recipes-app/static/js/runtime-main.346c1c57.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/project-recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "17d4af50699d3787904037a8ed312783",
+    "url": "/project-recipes-app/static/media/dice-svgrepo-com.17d4af50.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "84639ddb7a7bed347b38f18390db2052",
+    "url": "/project-recipes-app/static/media/drinks-bottles-svgrepo-com.84639ddb.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "d5adcf9d5aab7c5a1808d7a7f7d9fc90",
+    "url": "/project-recipes-app/static/media/ingredients-svgrepo-com.d5adcf9d.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/project-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/project-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/project-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
